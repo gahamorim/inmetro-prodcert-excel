b20fcf9122479cca306211b754fe69c011ddd680
Gabriel Amorim <gaahamorim95@gmail.com>
2024-12-11 23:00:13 -0300

    fix: headless chrome

--- scrapeInmetroProdCert.js
+++ scrapeInmetroProdCert.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 async function scrapeInmetro() {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   const inmetroProdutosCertificados = "http://www.inmetro.gov.br/prodcert/produtos/busca.asp";
 
